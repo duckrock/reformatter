b4e5ca16e9c718bdbc200b7968e14faa88d9e5bd
jcdf <tdksa90@gmail.com>
2023-09-25 14:08:26 -0700

    Delete reformatter directory

--- reformatter/UploadToGoogleDriveApril.py
+++
@@ -1,6 +0,0 @@
-POST https://www.googleapis.com/upload/drive/v3/files?uploadType=media HTTP/1.1
-Content-Type: application/vnd.google-apps.document
-Content-Length: [NUMBER_OF_BYTES_IN_FILE]
-Authorization: Bearer 	ot9iA41w-RiacdESd2j4hUZb
-
-[JPEG_DATA]
--- reformatter/testbold.py
+++
@@ -1,6 +0,0 @@
-test = 'tester.rtf'
-out_file = open(test,'w')
-out_file.write("""{\\rtf1
-This is \\b Bold  \\b0\line\
-}""")
-out_file.close() #thanks to the comment below
